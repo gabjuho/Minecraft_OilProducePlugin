c50c85a4bb59950dc48079c260c15a3aa11bd418
gabjuho <gabjuho@gmail.com>
2022-06-22 14:46:56 +0900

    feat: Add managing coolTime.yml method

--- src/main/java/com/gabjuho/oilproduceplugin/YamlManager.java
+++ src/main/java/com/gabjuho/oilproduceplugin/YamlManager.java
@@ -13,12 +13,15 @@ public class YamlManager {
     private final Main plugin; // 메인클래스 객체
     private static YamlManager instance = new YamlManager(Main.getPlugin(Main.class)); //yaml 매니저는 싱글톤으로 생성
     private FileConfiguration loc = null;
+    private FileConfiguration coolTime = null;
     private File locFile = null;
+    private File coolTimeFile = null;
 
     private YamlManager(Main plugin) // 생성자 막아놓음 (싱글톤 패턴으로 인한)
     {
         this.plugin = plugin;
         saveDefaultLoc();
+        saveDefaultCoolTime();
     }
 
     public void reloadLoc() // 데이터 리로드
@@ -32,6 +35,20 @@ public class YamlManager {
         {
             YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
             this.loc.setDefaults(defaultConfig);
+        }
+    }
+
+    public void reloadCoolTime() // 데이터 리로드
+    {
+        if(this.coolTimeFile == null)
+            this.coolTimeFile = new File(this.plugin.getDataFolder(), "coolTime.yml");
+
+        this.coolTime = YamlConfiguration.loadConfiguration(this.coolTimeFile);
+        InputStream defaultStream = this.plugin.getResource("loc.yml");
+        if(defaultStream != null)
+        {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.coolTime.setDefaults(defaultConfig);
         }
     }
 
@@ -50,6 +67,14 @@ public class YamlManager {
         return this.loc;
     }
 
+    public FileConfiguration getCoolTime()
+    {
+        if(this.coolTime == null)
+            reloadCoolTime();
+
+        return this.coolTime;
+    }
+
     public void saveLoc() // 데이터 저장
     {
         if(this.loc == null || this.locFile == null)
@@ -63,6 +88,19 @@ public class YamlManager {
             plugin.getLogger().log(Level.SEVERE,"data 파일이 저장되지 않았습니다. -> " + this.locFile,e);
         }
     }
+    public void saveCoolTime() // 데이터 저장
+    {
+        if(this.coolTime == null || this.coolTimeFile == null)
+            return;
+
+        try
+        {
+            this.getCoolTime().save(this.coolTimeFile);
+        }catch(IOException e)
+        {
+            plugin.getLogger().log(Level.SEVERE,"data 파일이 저장되지 않았습니다. -> " + this.coolTimeFile,e);
+        }
+    }
 
     public void saveDefaultLoc() // 데이터 디폴트 저장
     {
@@ -72,6 +110,17 @@ public class YamlManager {
         if(!this.locFile.exists())
         {
             this.plugin.saveResource("loc.yml",false);
+        }
+    }
+
+    public void saveDefaultCoolTime() // 데이터 디폴트 저장
+    {
+        if(this.coolTimeFile == null)
+            this.coolTimeFile = new File(this.plugin.getDataFolder(), "coolTime.yml");
+
+        if(!this.coolTimeFile.exists())
+        {
+            this.plugin.saveResource("coolTime.yml",false);
         }
     }
 
