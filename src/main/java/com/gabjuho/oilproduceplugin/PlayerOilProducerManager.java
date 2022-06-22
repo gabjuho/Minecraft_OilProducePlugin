3e60d8f4ce79be4c6344021ca3574039ec5f9f7d
gabjuho <gabjuho@gmail.com>
2022-06-22 17:31:38 +0900

    feat: Add giving oil to player system
    If player works oil producer, the system gives oil to player when cool time is done.
    But if player has no slot in own inventory, the system send specific message to player and player can't receive oil.

--- src/main/java/com/gabjuho/oilproduceplugin/PlayerOilProducerManager.java
+++ src/main/java/com/gabjuho/oilproduceplugin/PlayerOilProducerManager.java
@@ -1,5 +1,12 @@
 package com.gabjuho.oilproduceplugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
@@ -8,7 +15,6 @@ import java.util.UUID;
 public class PlayerOilProducerManager {
     private final static HashMap<UUID,Integer> oilCoolTime = new HashMap<>();
     private final static HashMap<UUID, BukkitTask> coolTimeTask = new HashMap<>();
-    private final static YamlManager yamlManager = YamlManager.getInstance();
 
     private PlayerOilProducerManager(){
     }
@@ -19,6 +25,40 @@ public class PlayerOilProducerManager {
             coolTimeTask.get(uuid).cancel();
             coolTimeTask.remove(uuid);
             oilCoolTime.remove(uuid);
+
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+            if(offlinePlayer.isOnline()){
+                Player player = Bukkit.getPlayer(uuid);
+                if(player != null) {
+                    ItemStack oil = new ItemStack(Material.COAL);
+                    ItemMeta oilMeta = oil.getItemMeta();
+                    oilMeta.setDisplayName("석유");
+                    oil.setItemMeta(oilMeta);
+                    Inventory inv = player.getInventory();
+                    boolean isEmpty = false;
+                    if(inv.firstEmpty() == -1){
+                        for (ItemStack invItem : inv.getContents()) {
+                            if(invItem == null)
+                                continue;
+                            if(!invItem.hasItemMeta())
+                                continue;
+                            if(invItem.getType() == oil.getType() && invItem.getItemMeta().equals(oil.getItemMeta()) && invItem.getAmount() < invItem.getMaxStackSize()){
+                                player.getInventory().addItem(oil);
+                                player.sendMessage(ChatColor.GREEN + "석유가 생산되어 "+player.getName()+"님의 인벤토리에 들어갔습니다.");
+                                isEmpty = true;
+                                break;
+                            }
+                        }
+                        if(!isEmpty)
+                            player.sendMessage(ChatColor.RED + player.getName() + "님의 인벤토리가 꽉 찾으므로, 생산된 석유가 지급되지 않았습니다.");
+                    }else {
+                        player.getInventory().addItem(oil);
+                        player.sendMessage(ChatColor.GREEN + "석유가 생산되어 "+player.getName()+"님의 인벤토리에 들어갔습니다.");
+                    }
+
+
+                }
+            }
             return;
         }
         coolTime -= 1;
